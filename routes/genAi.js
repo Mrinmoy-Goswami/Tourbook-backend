c093a5948e87669a874c18564d13379653efe335
Mrinmoy Goswami <you@example.com>
2024-02-13 21:05:16 +0530

    added AI feature

--- routes/genAi.js
+++ routes/genAi.js
@@ -23,17 +23,17 @@ router.post('/',async(req,res)=>{
     }
     })
 
-async function run() {
-  // For text-only input, use the gemini-pro model
-  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+// async function run() {
+//   // For text-only input, use the gemini-pro model
+//   const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
-  const prompt = "Write a story about a magic backpack."
+//   const prompt = "Write a story about a magic backpack."
 
-  const result = await model.generateContent(prompt);
-  const response =  result.response;
-  const text = response.text();
-  console.log(text);
-}
+//   const result = await model.generateContent(prompt);
+//   const response =  result.response;
+//   const text = response.text();
+//   console.log(text);
+// }
 
 module.exports = router
 
